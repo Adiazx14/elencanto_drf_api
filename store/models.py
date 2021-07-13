5bf18b8615890142e3238af337455c69ece2d9d7
Adiazx14 <adrianvaldes1439@gmail.com>
2021-07-12 23:11:29 -0400

    created category model

--- store/models.py
+++ store/models.py
@@ -2,3 +2,11 @@ from django.db import models
 from django.db.models.fields.files import ImageField
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    icon = ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
