24f5a396072ad582166c38f98aa08a3a87bd31ac
Adiazx14 <adrianvaldes1439@gmail.com>
2021-07-21 18:25:44 -0400

    added permision classes to views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from .serializers import CategorySerializer, ProductSerializer, MyTokenObtainPairSerializer, UserSerializer
 from .models import Category, Product
 from django.http import HttpResponse
@@ -5,6 +6,7 @@ from rest_framework.views import APIView, Response
 from rest_framework.generics import ListAPIView
 from rest_framework.views import status
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -21,7 +23,14 @@ class ProductsView(ListAPIView):
     serializer_class = ProductSerializer
 
 
+class UsersView(ListAPIView):
+    permission_classes = [IsAdminUser]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
 class UserView(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         user = request.user
