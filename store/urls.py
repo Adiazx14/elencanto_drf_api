0d60742b533516f6655864b48023f5d4b216a433
Adiazx14 <adrianvaldes1439@gmail.com>
2021-07-20 23:16:01 -0400

    simple-jwt setup

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import CategoriesView, ProductsView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
+    path('user/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('categories/', view=CategoriesView.as_view()),
     path('products/', view=ProductsView.as_view())
 
