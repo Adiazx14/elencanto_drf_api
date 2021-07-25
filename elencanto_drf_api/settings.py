a6a8dbdf3dc11eef00925549f7a57bfa219724d4
Adiazx14 <adrianvaldes1439@gmail.com>
2021-07-24 22:24:45 -0400

    8th

--- elencanto_drf_api/settings.py
+++ elencanto_drf_api/settings.py
@@ -159,13 +159,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/staticfiles/"
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
 
+]
+
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_ROOT = BASE_DIR / 'staticfiles/images'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
