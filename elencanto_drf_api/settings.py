600266dc41a9ed2959c505bef84d09c2f3a4430c
Adiazx14 <adrianvaldes1439@gmail.com>
2021-07-24 22:39:42 -0400

    9th

--- elencanto_drf_api/settings.py
+++ elencanto_drf_api/settings.py
@@ -175,7 +175,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 # aws s3
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
