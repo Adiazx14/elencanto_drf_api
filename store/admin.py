576374c594da4c58e9d3aa955fe166c7ced8bbc0
Adiazx14 <adrianvaldes1439@gmail.com>
2021-09-09 18:12:04 -0400

    superuser

--- store/admin.py
+++ store/admin.py
@@ -31,15 +31,3 @@ admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ShippingAddress)
 admin.site.register(Order, OrderAdmin)
-
-class MyAdminSite(AdminSite):
-    # Text to put at the end of each page's <title>.
-    site_title = ugettext_lazy('My site admin')
-
-    # Text to put in each page's <h1> (and above login form).
-    site_header = ugettext_lazy('My administration')
-
-    # Text to put at the top of the admin index page.
-    index_title = ugettext_lazy('Site administratin')
-
-admin_site = MyAdminSite()
